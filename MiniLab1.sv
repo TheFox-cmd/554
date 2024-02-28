8ea29959c5e48b1fc9f5efb4257f16c199db838e
JonathanYang-yzh <76476055+JonathanYang-yzh@users.noreply.github.com>
2024-02-27 18:05:28 -0600

    Add files via upload

--- MiniLab1.sv
+++ MiniLab1.sv
@@ -13,27 +13,19 @@ logic iorw_n, iocs_n;
 logic tx_q_full, rx_q_empty;
 logic rst_n;
 
+//assign specific isns to the part we want to implement
 assign iocs_n = ~&addr[15:14];
 
 assign dbus = (~iocs_n & ~iorw_n) ? wdata[7:0] : 8'hzz;
-
+//instantiation of CPU
 cpu iCPU(.clk(clk),.rst_n(rst_n),.wdata(wdata),.we(we),.rdata(rdata),.re(re),.addr(addr));
-
+//Reset synchronization
 rst_synch iRST(.clk(clk), .RST_n(RST_n), .rst_n(rst_n));
-
+//SPART instantiation
 spart iSP(.clk(clk), .rst_n(rst_n), .iocs_n(iocs_n), .iorw_n(iorw_n), .tx_q_full(tx_q_full), .rx_q_empty(rx_q_empty), .ioaddr(addr[1:0]), .databus(dbus), .TX(TX), .RX(RX));
 
+//logical assign to read/write enable
 assign iorw_n = re | ~we; 
-/*
-always_ff @(posedge clk, negedge rst_n)
-    if(!rst_n)
-        LEDR <= '0;
-    else
-        if(addr == 16'hC000 && we)
-            LEDR <= wdata[9:0];
-*/
 
-
-// assign LEDR = SW; 
-				
+                
 endmodule
